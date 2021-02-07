58e8e712a0f9c1694fb7b95232507eab7f80baf1
AKS Techies <akstechies@gmail.com>
2021-02-07 21:53:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from './components/layout/Header'
 import './App.css';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import {v4 as uuid} from 'uuid';
 
-function App() {
+//function App() {
+
+    class App extends Component {
+
+
+    //React.state = {
+        state = {
+        todos: [
+            {
+                id: uuid(),
+                title: 'task 1',
+                completed: false
+            },
+            {
+                id: uuid(),
+                title: '2 task',
+                completed: true
+            },
+            {
+                id: uuid(),
+                title: 'last task',
+                completed: false
+            },
+        ]
+    }
+
+    markComplete = (id) => {
+        this.setState({ todos: this.state.todos.map(todo => {
+            if(todo.id === id) {
+                todo.completed = !todo.completed
+            }
+            return todo
+        }) })
+    }
+
+    delTodo = (id) => {
+        this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id) ]
+        }) }
+
+    addTodo = (title) => {
+        //console.log(title)
+
+        const newTodo = {
+            id: uuid(),
+            title,
+            completed: false
+        }
+        this.setState({ todos: [...this.state.todos, newTodo ]});
+         
+    }
+    
+    
+    render() {
+    //console.log(React.state.todos[0])
+    //console.log(this.state.todos[0])
   return (
+<Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header />
+
+        
+
+        <AddTodo addTodo={this.addTodo} />
+        
+      <Todos todos={ this.state.todos } markComplete={this.markComplete} delTodo={this.delTodo} />
     </div>
+    </Router>
   );
+  
+}
 }
 
 export default App;
